d722207118374233b3441cf6ab7ce9a11b9b2440
Mustafa Eray <meraykeskin@gmail.com>
2022-11-26 21:11:06 +0300

    Test customer service subscirbe event function

--- customer/src/services/customer-service.js
+++ customer/src/services/customer-service.js
@@ -136,6 +136,9 @@ class CustomerService {
       case 'CREATE_ORDER':
         this.ManageOrder(userId, order);
         break;
+      case 'TEST':
+        console.log('WORKING... SUBSCRIBER');
+        break;
       default:
         break;
     }
