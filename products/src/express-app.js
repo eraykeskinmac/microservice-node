6767c794b22f8ecfed8ea511e6eb3fe029ecb9da
Mustafa Eray <meraykeskin@gmail.com>
2022-11-27 23:08:02 +0300

    Add product service app event and fix file

--- products/src/express-app.js
+++ products/src/express-app.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const cors  = require('cors');
-const { customer, products, shopping } = require('./api');
-const HandleErrors = require('./utils/error-handler')
+const cors = require('cors');
+const { products, appEvents } = require('./api');
+const HandleErrors = require('./utils/error-handler');
 
+module.exports = async app => {
+  app.use(express.json({ limit: '1mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+  app.use(cors());
+  app.use(express.static(__dirname + '/public'));
 
-module.exports = async (app) => {
+  // Listeners
+  appEvents(app);
 
-    app.use(express.json({ limit: '1mb'}));
-    app.use(express.urlencoded({ extended: true, limit: '1mb'}));
-    app.use(cors());
-    app.use(express.static(__dirname + '/public'))
+  //api
+  products(app);
 
-    //api
-    customer(app);
-    products(app);
-    shopping(app);
-
-    // error handling
-    app.use(HandleErrors);
-    
-}
+  // error handling
+  app.use(HandleErrors);
+};
