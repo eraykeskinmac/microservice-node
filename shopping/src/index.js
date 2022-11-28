b2729da9e9cade13b6edf0fa6dfaeab941af4a8a
Mustafa Eray <meraykeskin@gmail.com>
2022-11-29 00:09:13 +0300

    prettier formatting

--- shopping/src/index.js
+++ shopping/src/index.js
@@ -3,21 +3,21 @@ const { PORT } = require('./config');
 const { databaseConnection } = require('./database');
 const expressApp = require('./express-app');
 
-const StartServer = async() => {
+const StartServer = async () => {
+  const app = express();
 
-    const app = express();
-    
-    await databaseConnection();
-    
-    await expressApp(app);
+  await databaseConnection();
 
-    app.listen(PORT, () => {
-        console.log(`listening to port ${PORT}`);
+  await expressApp(app);
+
+  app
+    .listen(PORT, () => {
+      console.log(`listening to port ${PORT}`);
     })
-    .on('error', (err) => {
-        console.log(err);
-        process.exit();
-    })
-}
+    .on('error', err => {
+      console.log(err);
+      process.exit();
+    });
+};
 
 StartServer();
