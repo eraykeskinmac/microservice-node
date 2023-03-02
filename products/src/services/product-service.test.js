e05d6888769595a99216f182366daa53f1579224
Mustafa Eray <meraykeskin@gmail.com>
2023-03-02 20:46:15 +0300

    Add test case configurations

---
+++ products/src/services/product-service.test.js
@@ -0,0 +1,8 @@
+describe('ProductService', () => {
+  // which function
+  describe('CreateProduct', () => {
+    //which scenario we are testing
+    test('validate user inputs', () => {});
+    test('validate response', () => {});
+  });
+});
