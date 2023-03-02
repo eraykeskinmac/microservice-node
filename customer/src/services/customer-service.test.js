c3a8aec0788cdb0803775d68d09a68326c6ad11e
Mustafa Eray <meraykeskin@gmail.com>
2023-03-02 20:46:55 +0300

    Add test case configurations

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,8 @@
+describe('CustomerSevice', () => {
+  // which function
+  describe('SignIn', () => {
+    //which scenario we are testing
+    test('validate user inputs', () => {});
+    test('validate response', () => {});
+  });
+});
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,8 @@
+describe('ShoppingService', () => {
+  // which function
+  describe('PlaceOrder', () => {
+    //which scenario we are testing
+    test('validate user inputs', () => {});
+    test('validate response', () => {});
+  });
+});
